d21e36f268a57e646ca7a1a037ff140f3e71a70c
Samson Olusegun <olusegunsamson@live.com>
2023-05-03 19:21:04 +0100

    setup basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
 import { React, useState } from "react";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 import CurrencySwitcher from "./Components/CurrencySwitcher";
 import Display from "./Components/Display";
+import Home from "./Components/Home";
 import Form from "./Components/Form";
 
 import "./App.css";
 
 function App() {
-  const [currency, setCurrency] = useState("EUR");
+  // const [currency, setCurrency] = useState("EUR");
 
-  const handleCurrencyChange = () => {
-    //TODO: using traditional if else statement determine the new currency
-    setCurrency(currency === "EUR" ? "USD" : "EUR");
-  };
+  // const handleCurrencyChange = () => {
+  //   //TODO: using traditional if else statement determine the new currency
+  //   setCurrency(currency === "EUR" ? "USD" : "EUR");
+  // };
 
   return (
     <div>
-      <Display currency={currency} />
-      <CurrencySwitcher
-        handleChangeCurrency={handleCurrencyChange}
-        currency={currency}
-      />
-      <Form />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="form">Form</Link>
+      </nav>
+      <h1>This is a basic Router Setup
+      </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="form" element={<Form />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-
