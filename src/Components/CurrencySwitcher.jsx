6ef7f7b928e1ecd6865d0adca0b08242a135104d
Samson Olusegun <olusegunsamson@live.com>
2023-05-03 19:22:05 +0100

    setup rounting

--- src/Components/CurrencySwitcher.jsx
+++ src/Components/CurrencySwitcher.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+
 function CurrencySwitcher(props) {
-    return (
-        <button onClick={props.handleChangeCurrency}>
-          Current currency is {props.currency}. Change it!
-        </button>
-      )
+  return (
+    <div>
+      <button onClick={props.handleChangeCurrency}>
+        Current currency is {props.currency}. Change it!
+      </button>
+    </div>
+  );
 }
 
 export default CurrencySwitcher;
-
